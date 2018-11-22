ef589f07a5f5699c3a0fa3b6bff423a1e5397b09
oscar <oscar@oxkr.es>
2018-11-22 08:40:58 +0100

    first commit

---
+++ walkEach.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+const fileTemp = new Promise((resolve, reject) => {
+  fs.readFile("./famous.txt", "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      resolve(data);
+    }
+  });
+});
+
+let famous = [];
+
+function People(name, live, desc) {
+  this.name = name;
+  this.live = live;
+  this.desc = desc;
+}
+
+fileTemp.then(
+  result => {
+    let splited = result.split(/\r?\n/);
+    splited.forEach(people => {
+      const name = people.substring(0, people.indexOf("(")).trim();
+      const live = people
+        .substring(people.indexOf("(") + 1, people.indexOf(")"))
+        .trim();
+      const desc = people.substr(people.indexOf(")") + 1).trim();
+      famous.push(new People(name, live, desc));
+    });
+
+    fs.writeFile("famous.json", JSON.stringify(famous), "utf8", err => {
+      if (err) {
+        throw err;
+      }
+      console.log("Famous JSON created");
+    });
+
+    console.log(famous);
+  },
+  error => console.log(error)
+);
